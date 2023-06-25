0dd31b6d0a686c09f8795503bab1b4d02e1c795b
Noah Aliaga <noahjeremias31@icloud.com>
2023-06-24 21:36:44 -0400

    Replace old Invinc files with new

---
+++ State/Invincible/Invincible.cc
@@ -0,0 +1,64 @@
+#include "Invincible.hh"
+#include <cstdio>
+
+namespace State
+{
+    namespace Invincible
+    {
+        u8 InvincData::isInvinc(void)
+        {
+            if ((m_invincFrames != 0) || (m_wasHurt != 0) || (m_vulnerable != 0))
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        void InvincData::decrementFrames(void)
+        {
+            if (m_invincFrames < 1)
+            {
+                m_invincFrames = 0;
+                return;
+            }
+
+            m_invincFrames -= 1;
+            return;
+        }
+        
+        void InvincData::mightyEffect(void)
+        {
+            if (isInvinc() == 0)
+            {
+                m_mightyEffect = 0;
+                return;
+            }
+
+            m_mightyEffect = 1;
+            return;
+        }
+    }
+}
+
+int main(void)
+{
+    State::Invincible::InvincData *player;
+
+    player->m_invincFrames = 10;
+    player->m_wasHurt = 0;
+    player->m_vulnerable = 0;
+
+    for (u32 i = 0; i < 20; i++)
+    {
+        player->decrementFrames();
+        player->mightyEffect();
+        
+        printf("====================\n");
+        printf("Invinc Frames: %lu\n", player->m_invincFrames);
+        printf("Is Invinc: %hhu\n", player->isInvinc());
+        printf("Mighty Effect: %hhu\n", player->m_mightyEffect);
+    }
+
+    return 0;
+}
---
+++ State/Invincible/Invincible.hh
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../../Common/Types.hh"
+
+namespace State
+{    
+    namespace Invincible
+    {
+        class InvincData
+        {
+            public:
+                u32 m_refAddr;
+                u32 m_invincFrames;
+                u32 m_flashCycles;
+                u8 m_wasHurt;
+                u8 m_vulnerable;
+                u8 m_mightyEffect;
+            
+            u8 isInvinc(void);
+            void decrementFrames(void);
+            void mightyEffect(void);
+        };
+    }
+}
