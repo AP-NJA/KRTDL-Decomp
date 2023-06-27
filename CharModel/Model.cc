aeb289a127ed573e1137d9a9857670d761f56c5d
Noah Aliaga <noahjeremias31@icloud.com>
2023-06-26 21:15:13 -0400

    New model folder for model things

---
+++ CharModel/Model.cc
@@ -0,0 +1,9 @@
+#include "Model.hh"
+
+namespace CharModel
+{
+    u8 Model::isVisible(u8 m_isVisible)
+    {
+        this->m_isVisible = m_isVisible;
+    }
+}
---
+++ CharModel/Model.hh
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "../../Common/Types.hh"
+
+namespace CharModel
+{
+    class Model
+    {
+        private:
+            u8 m_isVisible;
+
+        u8 isVisible(u8 m_isVisible);
+    };
+}
