7c67c2a95a7aea934dbeb62fbd321ed4ed46d04b
AP_NJA <noahjeremias31@icloud.com>
2024-03-05 13:25:56 -0500

    Change datatype for FUN_8034f9cc

--- src/State/Intangible.hh
+++ src/State/Intangible.hh
@@ -12,7 +12,7 @@ typedef struct Intangible
 
 void intangibleDataConstructor(Intangible * param_1);
 u32 canSetIntangible(Intangible * param_1);
-void FUN_8034f9cc(Intangible * param_1);
+u32 FUN_8034f9cc(Intangible * param_1);
 void initializeIntangibleFrames(Intangible * param_1, u32 frames);
 void decrementIntangibleFrames(Intangible * param_1);
 void updateFlashCycle(Intangible * param_1);
