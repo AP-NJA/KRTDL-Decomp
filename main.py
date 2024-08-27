cb0399d235934435e149943bbedaefca916f92a8
Noah Aliaga <noahjeremias31@icloud.com>
2024-08-27 07:53:40 -0400

    Remove now useless Python script

--- main.py
+++
@@ -1,8 +0,0 @@
-import tools.project as project
-
-project.createContext()
-decompiler = project.Decompile()
-decompiler.askForFiles()
-decompiler.createObjectFiles()
-# decompiler.runDecompiler()
-project.runDiffer()
